b168663bfbd7dddaadcdf5a4b0284a9104ac70b1
yukitake <koshi.yukitake@gmail.com>
2023-06-18 05:57:19 +0900

    Update index.php

--- public/index.php
+++ public/index.php
@@ -44,14 +44,12 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-echo "Hello, App Runner PHP Managed Runtime!";
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
-// $app = require_once __DIR__.'/../bootstrap/app.php';
+$kernel = $app->make(Kernel::class);
 
-// $kernel = $app->make(Kernel::class);
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
 
-// $response = $kernel->handle(
-//     $request = Request::capture()
-// )->send();
-
-// $kernel->terminate($request, $response);
+$kernel->terminate($request, $response);
