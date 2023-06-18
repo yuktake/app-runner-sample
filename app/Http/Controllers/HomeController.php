0bb5eb7458f6bf74ca051ee9c8c0a9ab11fb33d5
yukitake <koshi.yukitake@gmail.com>
2023-06-18 20:20:29 +0900

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,9 +7,9 @@ use App\Models\User;
 class HomeController extends Controller {
 
     public function index() {
-        foreach (User::all() as $user) {
-            var_dump($user->name);
-        }
+        // foreach (User::all() as $user) {
+        //     var_dump($user->name);
+        // }
 
         return view('welcome');
     }
