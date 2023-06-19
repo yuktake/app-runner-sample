271b3cfbb7155829c3c6d68f74bd5a28d79cec1d
yukitake <koshi.yukitake@gmail.com>
2023-06-20 05:05:40 +0900

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends Controller {
         // foreach (User::all() as $user) {
         //     var_dump($user->name);
         // }
-        dd(DB::raw("SHOW TABLES"));
+        dd(DB::select("SHOW DATABASES"));
 
         return view('welcome');
     }
